9c2e5a98baed44baa8fe6b093bc48c94d1612e41
lazerskm <61406487+lazerskm@users.noreply.github.com>
2022-05-30 18:00:31 +1100

    correction chemin import

--- scripts/main.R
+++ scripts/main.R
@@ -1,10 +1,10 @@
-setwd("D:/Univ/L4/Analyse exploratoire/ACP---Analyse-exploratoire-des-donn-es/scripts/")
+setwd("D:/Univ/L4/Analyse exploratoire/ACP---Analyse-exploratoire-des-donn-es")
 
-source("generalist_cleaning.R")
-source("centrer_reduire.R")
-source("correlation.R")
+source("scripts/generalist_cleaning.R")
+source("scripts/centrer_reduire.R")
+source("scripts/correlation.R")
 
-Data <- read.csv("D:/Univ/L4/Analyse exploratoire/ACP---Analyse-exploratoire-des-donn-es/Car2DB_fra_cut.csv", sep = ";", encoding = "UTF-8", na.strings = c("", "NA", "#N/A", " "), dec = ",")
+Data <- read.csv("Car2DB_fra_cut.csv", sep = ";", encoding = "UTF-8", na.strings = c("", "NA", "#N/A", " "), dec = ",")
 Data <- clean(Data)
 
 
@@ -13,5 +13,3 @@ using_data <- Data[c("cylindree_cm3", "puissance_de_moteur_ch", "longueur_mm", "
 #comparaison de la fonction center réduire de R et de la notre
 sub_scale <- scale(using_data)
 sub_fonc <- centrer_reduire(using_data)
-
-all_regression_simple(using_data)
